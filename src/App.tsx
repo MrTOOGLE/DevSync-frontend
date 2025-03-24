ab9a770fa5fbeed3da2ddb51c8a2e4de9cada1e0
kizyakqq <groma480@gmail.com>
2025-03-24 16:42:22 +0700

    Создал форму без стилей и функционала

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import RegistrationForm from './pages/Auth/Registration.tsx'
+import Authorization from "./pages/Auth/Authorization.tsx";
 
 const App: React.FC = () => {
     return (
         <div className="app">
             <RegistrationForm />
+            <Authorization />
         </div>
     )
 };
--- src/pages/Auth/Authorization.tsx
+++ src/pages/Auth/Authorization.tsx
@@ -2,10 +2,30 @@ import React from 'react';
 
 const AuthorizationForm: React.FC = () => {
     return (
-        <div>
-            <p>Hello world</p>
+        <div className="authorizationFormContainer">
+            <h1>Войти</h1>
+            <form>
+                <div className="form_group">
+                    <input
+                        type="email"
+                        placeholder="Электронная почта*"
+                        required
+                    />
+                </div>
+                <div className="form_group">
+                    <input
+                        type="password"
+                        placeholder="Пароль*"
+                        required
+                    />
+                </div>
+                <p>
+                    У вас еще нет аккаунта? <a href="./Registration.tsx">Зарегистрироваться!</a>
+                </p>
+                <button className="sumbmit">Войти</button>
+            </form>
         </div>
-    )
-}
+    );
+};
 
 export default AuthorizationForm;
