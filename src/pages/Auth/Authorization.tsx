35ebf9a33f04a8fed6938709622b5c9e378dfe47
kizyakqq <groma480@gmail.com>
2025-03-24 17:55:49 +0700

    Добавил базовый функционал и чуть стилей

--- src/pages/Auth/Authorization.tsx
+++ src/pages/Auth/Authorization.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
+import React, {FormEvent, useState} from 'react';
+import './styles.css'
 
 const AuthorizationForm: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        console.log('Авторизация с почтой:', email, 'и паролем:', password);
+    };
+
     return (
         <div className="authorizationFormContainer">
-            <h1>Войти</h1>
-            <form>
-                <div className="form_group">
-                    <input
-                        type="email"
-                        placeholder="Электронная почта*"
-                        required
-                    />
+            <div className="authorizationForm">
+                <div className="heading">
+                    <h1>Войти</h1>
                 </div>
-                <div className="form_group">
-                    <input
-                        type="password"
-                        placeholder="Пароль*"
-                        required
-                    />
-                </div>
-                <p>
-                    У вас еще нет аккаунта? <a href="./Registration.tsx">Зарегистрироваться!</a>
-                </p>
-                <button className="sumbmit">Войти</button>
-            </form>
+                <form onSubmit={handleSubmit}>
+                    <div className="form_group">
+                        <input
+                            type="text"
+                            id="email"
+                            name="email"
+                            placeholder="Электронная почта*"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="form_group">
+                        <input
+                            type="text"
+                            id="password"
+                            name="password"
+                            placeholder="Пароль*"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <p>
+                        У вас еще нет аккаунта? <a href="#">Зарегистрироваться!</a>
+                    </p>
+                    <button type="submit">Войти</button>
+                </form>
+            </div>
         </div>
     );
 };
