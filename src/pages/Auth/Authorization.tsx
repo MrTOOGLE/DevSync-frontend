2e65049e172e342b4cb2f8f72208db27198652fd
kizyakqq <groma480@gmail.com>
2025-03-24 16:14:31 +0700

    Тестовый коммит, разбираюсь, как работать с этим всем

---
+++ src/pages/Auth/Authorization.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthorizationForm: React.FC = () => {
+    return (
+        <div>
+            <p>Привет</p>
+        </div>
+    )
+}
+
+export default AuthorizationForm;
